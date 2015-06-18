fe7c370d951771003f4d2886d2da635c19328210
cubapp <pi@pipeek.(none)>
2015-06-18 23:00:23 +0200

    Added relay support

--- ezs.py
+++ ezs.py
@@ -16,12 +16,17 @@ GPIO.setmode(GPIO.BCM) # attention!
 #pinid6 = 15
 # Pin Array
 piny  = [ 21, 23, 7, 11, 13, 15 ]
+relay = 4 
 # users array 
 users = {21: 'user000001', 23: 'user100000', 7: 'user000100', 11: 'user010000', 13: 'user001000', 15: 'user000010', }
 # tlacitko function 
 #   logs switch press and writes the log 
 def tlacitko(cislo):
     print time.ctime(), ",", time.time(), "," ,users[cislo]
+    # relay switch On/Off
+    GPIO.output(relay,True)
+    time.sleep(BT/300)
+    GPIO.output(relay,False)    
 
 # Main prog 
 # PIN settings        
@@ -30,6 +35,8 @@ for i in piny:
     GPIO.setup(i, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
     # Add handler to all detected switches
     GPIO.add_event_detect(i, GPIO.RISING, callback=tlacitko, bouncetime=BT)
+#Relay PIN
+GPIO.setup(relay, GPIO.OUT)
 
 # cycle
 while True:
