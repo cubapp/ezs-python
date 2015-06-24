1392d6ce2b7912ae773274eee2b2e8525af2ccc1
cubapp <pi@pipeek.(none)>
2015-06-24 13:17:35 +0200

    Redesign of PINs

--- ezs.py
+++ ezs.py
@@ -10,10 +10,17 @@ GPIO.setmode(GPIO.BCM) # attention!
 # the pins we are going to use (BOARD)
 # pinid1 = 21 #pinid2 = 23 #pinid3 = 7 #pinid4 = 11 #pinid5 = 13 #pinid6 = 15
 # Pin Array
-piny  = [ 21, 23, 7, 11, 13, 15 ]
+switchpins  = [ 21, 23, 7, 11, 13, 15 ]
 relay = 4 
 # users array 
-users = {21: 'user000001', 23: 'user100000', 7: 'user000100', 11: 'user010000', 13: 'user001000', 15: 'user000010', }
+users = {
+	21: 'user000001', 
+	23: 'user100000', 
+	7:  'user000100', 
+	11: 'user010000', 
+	13: 'user001000', 
+	15: 'user000010', 
+}
 # switchpress function 
 #   logs switch press and writes the log 
 def switchpress(innumber):
@@ -25,15 +32,17 @@ def switchpress(innumber):
 
 # Main prog 
 # PIN settings        
-for i in piny:
+# 1. Relay PIN
+GPIO.setup(relay, GPIO.OUT)
+# 2. Switch PINs - for all switchpins setu up as INUPT and register event_detect
+for i in switchpins:
     # Set Up All PINs to pull down
     GPIO.setup(i, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
     # Add handler to all detected switches
     GPIO.add_event_detect(i, GPIO.RISING, callback=switchpress, bouncetime=BT)
-#Relay PIN
-GPIO.setup(relay, GPIO.OUT)
+# END of setting PINs
 
-# cycle
+# Main and Only Cycle
 while True:
         print time.ctime()
         time.sleep(2)
